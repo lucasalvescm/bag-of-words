a68b828756bdf9e47468250603df390def036dda
lucasalvescm <lucasalves.s@outlook.com>
2017-09-14 13:01:09 -0300

    Novas alterações para montar matriz

--- bag_of_words.py
+++ bag_of_words.py
@@ -12,60 +12,54 @@ list_all_movies = [] #Lista com os nomes de todos os filmes
 list_filter_movies =[] #Lista com os nomes de todos os filmes sem repitir
 list_words_exclude = ['the','is','of','in','that','and','our'] #Palavras para desconsiderar
 list_filmes = []
-dict_words={}
-#Filtra o dataset
-for row in dsMovies:
-	vectorRow = row.split('::')
-	objRow={
-		'id': vectorRow[0],
-		'nome': vectorRow[1],
-		'genero': vectorRow[2]
-	}
-	list_filmes.append(objRow['nome'])
-	movie_name = vectorRow[1].lower()
-	movie_name = movie_name[0:len(movie_name) -7] #remove o ano do nome
-	movie_name = movie_name.split(' ') #separa o nome de cada filme
 
-	#Faz o filtro das palavras a desconsiderar e adiciona a lista de filmes
-	for name in movie_name:
-		if name not in list_words_exclude:
-			list_all_movies.append(name)
-			if name not in list_filter_movies:
-				list_filter_movies.append(name)
+#Filtra o dataset
+count = 0
+for row in dsMovies:
+	if count<3:
+		vectorRow = row.split('::')
+		objRow={
+			'id': vectorRow[0],
+			'nome': vectorRow[1],
+			'genero': vectorRow[2]
+		}
+		list_filmes.append(objRow['nome'])
+		movie_name = vectorRow[1].lower()
+		movie_name = movie_name[0:len(movie_name) -7] #remove o ano do nome
+		movie_name = movie_name.split(' ') #separa o nome de cada filme
+
+		#Faz o filtro das palavras a desconsiderar e adiciona a lista de filmes
+		for name in movie_name:
+			if name not in list_words_exclude:
+				list_all_movies.append(name)
+				if name not in list_filter_movies:
+					list_filter_movies.append(name)
+	else:
+		break
+
+	count +=1
 
 file = open('testfile.txt','w') 
 for palavra in list_filter_movies:
 	dict_unique = {}
-	count_word = 0
+	dict_words={}	
+	#print (palavra)
 	#import ipdb;ipdb.set_trace()
 	for filme in list_filmes:
+		#print(filme)
+		count_word = 0
 		titulo_split = filme.split(" ")
-		for titulo in titulo_split:
-			if palavra.lower() == titulo.lower():
-				count_word+=1
+		titulo_split = [element.lower() for element in titulo_split]
+		if palavra.lower() in titulo_split:
+			count_word = 1
+
 		dict_unique.update({filme.lower():count_word})
 	dict_words.update({palavra:dict_unique})
+
 	file.write(str(dict_words)+'\n')
 
 file.close()
 
-#print (dict_words)
-
-
-
-
-
-
-
-
-
-
-
-
-# print(list_filter_movies)
-# print(len(list_filter_movies))
-# print(list_all_movies)
-# print(len(list_all_movies))
 
 
 
